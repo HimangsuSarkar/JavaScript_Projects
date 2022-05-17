22c644e0cae4f72dd18acd2e44c30460ba471e6f
Himangsu <himangsusarkar1@gmail.com>
2022-05-17 22:43:01 +0800

    slove one problem

--- Jsbank/script.js
+++ Jsbank/script.js
@@ -68,7 +68,7 @@ containerMovements.innerHTML='';
   movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `<div class="movements__row">
-          <div class="movements__type movements__type--deposit">${i + 1} ${type}</div>
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
           <div class="movements__value">${mov}€</div>
         </div>
    
